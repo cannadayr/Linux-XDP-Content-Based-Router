2483d25cb5d10a447057de0d14994fd23a01393e
George Main IV <mainge@rose-hulman.edu>
2020-05-22 13:22:00 -0400

    fix indentation

--- kernel/udp-udp/udp.c
+++ kernel/udp-udp/udp.c
@@ -37,100 +37,100 @@ struct bpf_map_def SEC("maps") redirect = {
 SEC("xdp")
 int  xdp_prog_simple(struct xdp_md *ctx) {
 
-  char fmt[] = "Received message \"%s\", routing to machine %d\n";
-  char fwdError[] = "Forwarding Table Error %x\n";
-  char redirectError[] = "Redirect Map Lookup Error\n";
-  char ipError[] = "IP Map Lookup Error\n";
-  char txError[] = "TX Map Lookup Error\n";
+	char fmt[] = "Received message \"%s\", routing to machine %d\n";
+	char fwdError[] = "Forwarding Table Error %x\n";
+	char redirectError[] = "Redirect Map Lookup Error\n";
+	char ipError[] = "IP Map Lookup Error\n";
+	char txError[] = "TX Map Lookup Error\n";
  
-  void *data = (void *)(long)ctx->data;
-  void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+	void *data_end = (void *)(long)ctx->data_end;
 
-  struct ethhdr *eth = data;
-  if (eth + 1 > data_end) return XDP_PASS;
-  if (eth->h_proto == __constant_htons(ETH_P_IP)) {
+	struct ethhdr *eth = data;
+	if (eth + 1 > data_end) return XDP_PASS;
+	if (eth->h_proto == __constant_htons(ETH_P_IP)) {
   
-    struct iphdr * iph = data + sizeof(struct ethhdr);
-    if (iph + 1 > data_end) return XDP_PASS;
-    if (iph->protocol == IPPROTO_UDP) {
+		struct iphdr * iph = data + sizeof(struct ethhdr);
+    	if (iph + 1 > data_end) return XDP_PASS;
+    	if (iph->protocol == IPPROTO_UDP) {
 
-      struct udphdr *uh = (struct udphdr *) (iph + 1);
-      if (uh + 1 > data_end) return XDP_PASS;
-      if (MSB(uh->dest) == 7777) {
+			struct udphdr *uh = (struct udphdr *) (iph + 1);
+			if (uh + 1 > data_end) return XDP_PASS;
+			if (MSB(uh->dest) == 7777) {
 
-		uint32_t dataLocation = sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct udphdr);
-		if (data + dataLocation + 1 > data_end) return XDP_PASS; 
+			uint32_t dataLocation = sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct udphdr);
+			if (data + dataLocation + 1 > data_end) return XDP_PASS; 
 
-		char * udpDataStart = data + dataLocation;
-		char machine = udpDataStart[0];
-		char printMachine[] = "0";
-		printMachine[0] = machine;
+			char * udpDataStart = data + dataLocation;
+			char machine = udpDataStart[0];
+			char printMachine[] = "0";
+			printMachine[0] = machine;
 
-		__u32 *machineNumber = bpf_map_lookup_elem(&redirect, &machine);
-		if (!machineNumber) {
-			bpf_trace_printk(redirectError, sizeof(redirectError));
-			return XDP_ABORTED;
-		}
-		
-		__u32 hostMachine = 0;
-		bpf_trace_printk(fmt, sizeof(fmt), printMachine, *machineNumber); 
-
-		struct ip *destValue = bpf_map_lookup_elem(&xdp_server_ips, machineNumber);
-		struct ip *srcValue = bpf_map_lookup_elem(&xdp_server_ips, &hostMachine);
-		
-		if (!destValue || !srcValue) {
-			bpf_trace_printk(ipError, sizeof(ipError));
-			return XDP_ABORTED;
-		}
+			__u32 *machineNumber = bpf_map_lookup_elem(&redirect, &machine);
+			if (!machineNumber) {
+				bpf_trace_printk(redirectError, sizeof(redirectError));
+				return XDP_ABORTED;
+			}
 			
-		iph->daddr = destValue->addr; 
-		iph->saddr = srcValue->addr;
-	  
-		iph->check = 0;
-		
-		uint32_t sum = 0;
-		uint8_t * msg = data + sizeof(struct ethhdr);
-		
-		for (int i = 0; i < 20; i += 2) {
-			sum += (((uint16_t) msg[i]) << 8) + msg[i+1];
-		}
-		
-		sum = (sum >> 16) + (sum & 0xFFFF);
-		sum = (uint16_t) ~sum;
-		sum = ((sum & 0xFF00) >> 8) + ((sum & 0x00FF) << 8);
-		
-		iph->check = sum;
+			__u32 hostMachine = 0;
+			bpf_trace_printk(fmt, sizeof(fmt), printMachine, *machineNumber); 
 
-		int x = ctx->ingress_ifindex;
-		int *ifindex = bpf_map_lookup_elem(&tx_port, &x);
-		if (!ifindex) {
-			bpf_trace_printk(txError, sizeof(txError));
-			return XDP_ABORTED;
-		}
-	  
-		struct bpf_fib_lookup fib_params = {};
-		fib_params.family = 2;
-		fib_params.tos = iph->tos;
-		fib_params.l4_protocol=iph->protocol;
-		fib_params.sport = 0;
-		fib_params.dport = 0;
-		fib_params.tot_len = bpf_ntohs(iph->tot_len);
-		fib_params.ipv4_src = iph->saddr;
-		fib_params.ipv4_dst = iph->daddr;
-		fib_params.ifindex = *ifindex;
+			struct ip *destValue = bpf_map_lookup_elem(&xdp_server_ips, machineNumber);
+			struct ip *srcValue = bpf_map_lookup_elem(&xdp_server_ips, &hostMachine);
+			
+			if (!destValue || !srcValue) {
+				bpf_trace_printk(ipError, sizeof(ipError));
+				return XDP_ABORTED;
+			}
+				
+			iph->daddr = destValue->addr; 
+			iph->saddr = srcValue->addr;
 		
-		int rc = bpf_fib_lookup(ctx, &fib_params, sizeof(fib_params), 0);
+			iph->check = 0;
+			
+			uint32_t sum = 0;
+			uint8_t * msg = data + sizeof(struct ethhdr);
+			
+			for (int i = 0; i < 20; i += 2) {
+				sum += (((uint16_t) msg[i]) << 8) + msg[i+1];
+			}
+			
+			sum = (sum >> 16) + (sum & 0xFFFF);
+			sum = (uint16_t) ~sum;
+			sum = ((sum & 0xFF00) >> 8) + ((sum & 0x00FF) << 8);
+			
+			iph->check = sum;
 
-		if (rc == BPF_FIB_LKUP_RET_SUCCESS) {
-			__builtin_memcpy(eth->h_dest, fib_params.dmac, ETH_ALEN);
-			__builtin_memcpy(eth->h_source, fib_params.smac, ETH_ALEN);
-			return bpf_redirect_map(&tx_port, ctx->ingress_ifindex, 0);
+			int x = ctx->ingress_ifindex;
+			int *ifindex = bpf_map_lookup_elem(&tx_port, &x);
+			if (!ifindex) {
+				bpf_trace_printk(txError, sizeof(txError));
+				return XDP_ABORTED;
+			}
+		
+			struct bpf_fib_lookup fib_params = {};
+			fib_params.family = 2;
+			fib_params.tos = iph->tos;
+			fib_params.l4_protocol=iph->protocol;
+			fib_params.sport = 0;
+			fib_params.dport = 0;
+			fib_params.tot_len = bpf_ntohs(iph->tot_len);
+			fib_params.ipv4_src = iph->saddr;
+			fib_params.ipv4_dst = iph->daddr;
+			fib_params.ifindex = *ifindex;
+			
+			int rc = bpf_fib_lookup(ctx, &fib_params, sizeof(fib_params), 0);
+
+			if (rc == BPF_FIB_LKUP_RET_SUCCESS) {
+				__builtin_memcpy(eth->h_dest, fib_params.dmac, ETH_ALEN);
+				__builtin_memcpy(eth->h_source, fib_params.smac, ETH_ALEN);
+				return bpf_redirect_map(&tx_port, ctx->ingress_ifindex, 0);
+			}
+
+			bpf_trace_printk(fwdError, sizeof(fwdError), rc);
+			}
 		}
-
-		bpf_trace_printk(fwdError, sizeof(fwdError), rc);
-      }
 	}
-  }
   
   return XDP_PASS;
 }
